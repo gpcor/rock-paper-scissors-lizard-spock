9242d3438aa88cef40a6dbfb786256f8d0824178
Gabe Corsini <gcorsini@Gabes-MBP.fios-router.home>
2020-06-06 10:19:04 -0400

    Refactored to follow style guidelines

--- rpslsp.py
+++ rpslsp.py
@@ -1,3 +1,5 @@
+"""This program is for the  game Rock-paper-scissors-lizard-spock"""
+
 import random
 import sys
 
@@ -5,39 +7,42 @@ from time import sleep
 
 
 class MyGame:
-    throwOptions = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+    """The Game class for the program"""
+
+    throw_options = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
     def __init__(self):
 
         self.scoreboard = {'player': 0, 'computer': 0}
-        self.matchScore = {'player': 0, 'computer': 0}
-        self.playerThrow = ''
-        self.computerThrow = ''
+        self.match_score = {'player': 0, 'computer': 0}
+        self.player_throw = ''
+        self.computer_throw = ''
         self.new_game_answer = ''
-        self.rulesChoice = ''
+        self.rules_choice = ''
 
     def game_start(self):
-        print('''                                                       
-        88888888ba   88888888ba    ad88888ba   88           ad88888ba   
-        88      "8b  88      "8b  d8"     "8b  88          d8"     "8b  
-        88      ,8P  88      ,8P  Y8,          88          Y8,          
-        88aaaaaa8P'  88aaaaaa8P'  `Y8aaaaa,    88          `Y8aaaaa,    
-        88""""88'    88""""""'      `"""""8b,  88            `"""""8b,  
-        88    `8b    88                   `8b  88                  `8b  
-        88     `8b   88           Y8a     a8P  88          Y8a     a8P  
-        88      `8b  88            "Y88888P"   88888888888  "Y88888P"                                                       
+        '''Determines the start of the game and if rules are displayed'''
+        print('''
+        88888888ba   88888888ba    ad88888ba   88           ad88888ba
+        88      "8b  88      "8b  d8"     "8b  88          d8"     "8b
+        88      ,8P  88      ,8P  Y8,          88          Y8,
+        88aaaaaa8P'  88aaaaaa8P'  `Y8aaaaa,    88          `Y8aaaaa,
+        88""""88'    88""""""'      `"""""8b,  88            `"""""8b,
+        88    `8b    88                   `8b  88                  `8b
+        88     `8b   88           Y8a     a8P  88          Y8a     a8P
+        88      `8b  88            "Y88888P"   88888888888  "Y88888P"
         ''')
 
         print('Welcome to Rock-Paper-Scissors-Lizard-Spock')
         sleep(2)
         print('Basically Rock-Paper-Scissors but with more options')
         sleep(2)
-        print(
-            'Need to know the (r)ules? Or are you ready to (s)tart? (use "r" and "s" to either see the rules or start '
-            'the game)')
-        self.rulesChoice = str.lower(input())
+        print('Need to know the (r)ules? Or are you ready to (s)tart?')
+        print('(Use "r" and "s" to either see the rules or start the game):')
+        self.rules_choice = input()
 
     def game_rules(self):
+        '''Only job here is to display the rules'''
 
         game_rules = '''
             # Rock, Paper, Scissors, Lizard, Spock
@@ -64,105 +69,108 @@ class MyGame:
             Rules
             -----
             * All players will one make one throw per round
-            * The game is Best 3 out of 5 with a maximum of five rounds per Match
+            * The game is Best 3 out of 5 with a maximum of five rounds per
+              Match
             * The first to three points wins the Match
             '''
 
-        if self.rulesChoice == 'r':
+        if self.rules_choice == str.lower('r'):
             print(game_rules)
         else:
             pass
 
     def make_throws(self):
+        '''Player inputs their throw and the computer randomly selects its'''
 
         print('Make your throw.')
-        self.playerThrow = str.lower(input())
-        self.computerThrow = random.choice(self.throwOptions)
+        self.player_throw = str.lower(input())
+        self.computer_throw = random.choice(self.throw_options)
 
     def throw_match_up(self):
+        '''compares throws made by player and pc to see who won the throw'''
 
-        if self.playerThrow == 'rock':
+        if self.player_throw == 'rock':
 
-            if self.computerThrow == 'scissors' or self.computerThrow == 'lizard':
+            if self.computer_throw == 'scissors' or 'lizard':
                 print('{} trumps {}, you win the round!'.format(
-                    self.playerThrow, self.computerThrow))
+                    self.player_throw, self.computer_throw))
                 self.scoreboard['player'] += 1
                 print(self.scoreboard)
 
-            elif self.computerThrow == self.playerThrow:
+            elif self.computer_throw == self.player_throw:
                 print('The round is a tie!')
 
             else:
                 print('{} is trumped by {}, you lose the round'.format(
-                    self.playerThrow, self.computerThrow))
+                    self.player_throw, self.computer_throw))
                 self.scoreboard['computer'] += 1
                 print(self.scoreboard)
 
-        elif self.playerThrow == 'paper':
+        elif self.player_throw == 'paper':
 
-            if self.computerThrow == 'rock' or self.computerThrow == 'spock':
+            if self.computer_throw == 'rock' or 'spock':
                 print('{} trumps {}, you win the round!'.format(
-                    self.playerThrow, self.computerThrow))
+                    self.player_throw, self.computer_throw))
                 self.scoreboard['player'] += 1
                 print(self.scoreboard)
 
-            elif self.computerThrow == self.playerThrow:
+            elif self.computer_throw == self.player_throw:
                 print('The round is a tie!')
 
             else:
                 print('{} is trumped by {}, you lose the round'.format(
-                    self.playerThrow, self.computerThrow))
+                    self.player_throw, self.computer_throw))
                 self.scoreboard['computer'] += 1
                 print(self.scoreboard)
 
-        elif self.playerThrow == 'scissors':
+        elif self.player_throw == 'scissors':
 
-            if self.computerThrow == 'paper' or self.computerThrow == 'lizard':
+            if self.computer_throw == 'paper' or 'lizard':
                 print('{} trumps {}, you win the round!'.format(
-                    self.playerThrow, self.computerThrow))
+                    self.player_throw, self.computer_throw))
                 self.scoreboard['player'] += 1
                 print(self.scoreboard)
 
-            elif self.computerThrow == self.playerThrow:
+            elif self.computer_throw == self.player_throw:
                 print('The round is a tie!')
 
             else:
                 print('{} is trumped by {}, you lose the round'.format(
-                    self.playerThrow, self.computerThrow))
+                    self.player_throw, self.computer_throw))
                 self.scoreboard['computer'] += 1
                 print(self.scoreboard)
 
-        elif self.playerThrow == 'lizard':
+        elif self.player_throw == 'lizard':
 
-            if self.computerThrow == 'paper' or self.computerThrow == 'spock':
+            if self.computer_throw == 'paper' or 'spock':
                 print('{} trumps {}, you win the round!'.format(
-                    self.playerThrow, self.computerThrow))
+                    self.player_throw, self.computer_throw))
                 self.scoreboard['player'] += 1
                 print(self.scoreboard)
 
-            elif self.computerThrow == self.playerThrow:
+            elif self.computer_throw == self.player_throw:
                 print('The round is a tie!')
 
             else:
                 print('{} is trumped by {}, you lose the round'.format(
-                    self.playerThrow, self.computerThrow))
+                    self.player_throw, self.computer_throw))
                 self.scoreboard['computer'] += 1
                 print(self.scoreboard)
 
-        elif self.playerThrow == 'spock':
+        elif self.player_throw == 'spock':
 
-            if self.computerThrow == 'rock' or self.computerThrow == 'scissors':
+            if self.computer_throw == 'rock' or 'scissors':
                 print('{} trumps {}, you win the round!'.format(
-                    self.playerThrow, self.computerThrow))
+                    self.player_throw, self.computer_throw))
                 self.scoreboard['player'] += 1
                 print(self.scoreboard)
 
-            elif self.computerThrow == self.playerThrow:
+            elif self.computer_throw == self.player_throw:
                 print('The round is a tie!')
 
             else:
                 print('{} is trumped by {}, you lose the round'.format(
-                    self.playerThrow, self.computerThrow))
+                    self.player_throw, self.computer_throw))
                 self.scoreboard['computer'] += 1
                 print(self.scoreboard)
 
@@ -170,26 +178,32 @@ class MyGame:
             print('That is not an approve throw.')
 
     def new_game_question(self):
+        '''Determines if a new game will be played or not'''
 
         print('Thanks for playing! Wanna go again? ')
 
         while True:
 
             self.new_game_answer = str.lower(input())
-            if self.new_game_answer == 'yes':
+
+            if self.new_game_answer != 'yes' or 'no':
+                print('''I prefer yes and no answers only,
+                please and thank you...''')
+
+            elif self.new_game_answer == 'yes':
+
                 for i in iter(self.scoreboard):
                     self.scoreboard[i] = 0
                 print('Let us begin...')
                 break
-
-            elif self.new_game_answer != 'yes' and self.new_game_answer != 'no':
-                print('I prefer yes and no answers only, please and thank you...')
 
             else:
                 break
 
 
 def main():
+    '''Assembles game logic and win conditions'''
+
     game = MyGame()
     game.game_start()
     game.game_rules()
@@ -201,8 +215,8 @@ def main():
         if game.scoreboard['player'] == 3:
 
             print('You win!')
-            game.matchScore['player'] += 1
-            print('The current match score is {}'.format(game.matchScore))
+            game.match_score['player'] += 1
+            print('The current match score is {}'.format(game.match_score))
             game.new_game_question()
             try:
                 if game.new_game_answer == 'yes':
@@ -212,16 +226,16 @@ def main():
                 else:
                     print('See you later')
                     sys.exit()
-            except:
-                if KeyboardInterrupt:
+            except KeyboardInterrupt:
+                if KeyboardInterrupt is True:
                     print('How abruptly rude...')
                     sys.exit()
 
         elif game.scoreboard['computer'] == 3:
 
             print('The computer won!')
-            game.matchScore['computer'] += 1
-            print('The current match score is {}'.format(game.matchScore))
+            game.match_score['computer'] += 1
+            print('The current match score is {}'.format(game.match_score))
             game.new_game_question()
 
             if game.new_game_answer == 'yes':
